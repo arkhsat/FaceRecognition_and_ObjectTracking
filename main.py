835307a8beaf1a9b6864f89fb64e57f68095afd9
arkhsat <103724563+arkhsat@users.noreply.github.com>
2024-10-04 14:43:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ from displaycapture import display_image_from_url
 # Firebase setup
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://testing1-5b399-default-rtdb.firebaseio.com/",
-    'storageBucket': "testing1-5b399.appspot.com"
+    
 })
 
 # Webcam setup
